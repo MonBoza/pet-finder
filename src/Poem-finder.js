ea463c2ba357922b1ed95b9b589ff3d04199e283
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-16 17:25:29 -0600

    update CSS and index.js
    Co-authored-by: Zuri Gallegos <ZuriGa@users.noreply.github.com>

--- src/Poem-finder.js
+++ src/Poem-finder.js
@@ -1,5 +1,15 @@
-export default class PetFinder {
-    static getPetFinder(animal) {
-        return fetch()
+export default class PoemFinder {
+    static async getPoem(keyword) {
+        try{
+        const response = await fetch(`https://poetrydb.org/title/${keyword}`)
+        const jsonResponse = await response.json();
+            if (!response.ok) {
+                const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
+                throw new Error(errorMessage);
+            }
+            return jsonResponse;
+          } catch(error) {
+            return error;
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import PoemFinder from './Poem-finder';
+
+// Business Logic
+async function getPoem(keyword) {
+    const response = await PoemFinder.getPoem(keyword);
+    if (response) {
+        printElements(response, keyword);
+    } else {
+        printError(response, keyword);
+    }
+}
+
+// UI Logic
+
+function printElements(response, keyword) {
+    document.querySelector('#displayResults').innerText = `Here are the poems with the word ${keyword}`
+    const resultContainer = document.querySelector('#displayResults');
+    response.forEach(function (poem){
+        const listItem = document.createElement("li");
+        listItem.textContent = `Title: ${poem.title} | Author: ${poem.author} | Lines: ${poem.lines}`;
+        resultContainer.appendChild(listItem);
+    });
+}
+
+function printError(error, keyword) {
+  let errorMessage = '';
+  
+  if (!keyword || !isValidKeyword(keyword)) {
+    errorMessage = `Invalid keyword: ${keyword}. Please enter a valid keyword.`;
+  } else {
+    errorMessage = `There was an error accessing the poem data for ${keyword}: ${error}`
+  }
+    document.querySelector('#displayResults').innerText = errorMessage;
+}
+
+function isValidKeyword(keyword) {
+  return keyword.length >= 3;
+}
+
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const keyword = document.querySelector('#keyWord').value;
+    document.querySelector('#keyWord').value;
+    getPoem(keyword);
+}
+
+window.addEventListener("load", function () {
+    document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
